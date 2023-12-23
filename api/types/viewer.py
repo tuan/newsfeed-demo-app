c3f88f89b27163adcfd2c16e3701867e3070f7a0
Tuan Nguyen <tuan@users.noreply.github.com>
2023-12-22 20:23:29 -0800

    Implements newsfeedStoried field in viewer

--- api/types/viewer.py
+++ api/types/viewer.py
@@ -1,7 +1,9 @@
-from typing import Sequence
+from typing import Iterable, Sequence
 import strawberry
+from strawberry import relay
 from api.types.actor import Actor
 from api.types.person import Person
+from api.types.story import Story
 from api.utils.actor import convert_to_actor
 
 from db import db
@@ -28,3 +30,7 @@ class Viewer:
             and node["id"] != "the-viewer"
             and (search.lower() in node["name"].lower() if search is not None else True)
         ]
+
+    @relay.connection(relay.ListConnection[Story])
+    def newsfeed_stories(self) -> Iterable[Story]:
+        return (Story(node) for node in db.nodes if node["__typename"] == "Story")
